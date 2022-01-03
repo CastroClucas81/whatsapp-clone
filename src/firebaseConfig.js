51228b9c4b8f212a50310799ecdcb7b4e6f0e9b0
CastroClucas81 <68257824+CastroClucas81@users.noreply.github.com>
2022-01-02 23:27:55 -0300

    change firebase config

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,9 +1,9 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-  apiKey: "AIzaSyDPLHktetUbc2305p_UbWM5wNvtoNTKv_M",
-  authDomain: "whatsapp-clone-1cd7e.firebaseapp.com",
-  projectId: "whatsapp-clone-1cd7e",
-  storageBucket: "whatsapp-clone-1cd7e.appspot.com",
-  messagingSenderId: "35293552595",
-  appId: "1:35293552595:web:ae5cd477136c8f53c99926",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
